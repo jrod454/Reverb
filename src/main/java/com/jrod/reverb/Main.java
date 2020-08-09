8f45d3c8ea4ddcd3ff78f179d1b20abc00280936
jrod454 <jrod454@gmail.com>
2020-08-09 17:17:09 -0500

    Updated the display name.

--- src/main/java/com/jrod/reverb/Main.java
+++ src/main/java/com/jrod/reverb/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("app.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Reverb");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
